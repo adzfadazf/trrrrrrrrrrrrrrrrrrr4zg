c7a6d48e505d9211208a045c31bf9041bee5515b
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-05 18:51:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,9 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('ready', () => {
+  let channel = client.channels.get('442803314531500053');
+  channel.join()
 });
 
 // THIS  MUST  BE  THIS  WAY
