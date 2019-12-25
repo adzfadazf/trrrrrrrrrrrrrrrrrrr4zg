45850bb8f54d5a85dd870a1d6e908bca5b9dd460
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-25 13:48:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 
   client.on("ready", () => {
-  const channel = client.channels.get("627884388452925453");
+  const channel = client.channels.get("529746544975544342");
   if (!channel) return console.error("Can't find the channel.");
   channel.join().then(connection => {
     console.log("Connected.[Done]");
