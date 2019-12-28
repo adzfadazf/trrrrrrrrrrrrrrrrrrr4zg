921e42a85deb662cacbb072aae44f3e48f269230
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-28 19:18:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-console.log(`Logged in as ${client.user.tag}!`);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
