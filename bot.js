264eb92aac87640039d063dfbbbde137500490b9
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-29 18:08:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const client2 = new Discord.Client();
-
 
 client.on("ready", () => {
   const channel = client.channels.get("627884388452925453");
@@ -12,5 +10,4 @@ client.on("ready", () => {
     console.error(error_);
   });
 });
-client.login("NjU5NDA1MzY2NjI2NjE1MzE3.XgjZfg.V5a_GNyI_jBkr19BOhZRASmWCcs")
-client2.login("NjU5NDI1ODk2MDQ1NzQwMDYy.Xgjb6g.o5VjsWULq7kd--pthJA1Lb8z0zA")
+client.login(process.env.BOT_TOKEN);
