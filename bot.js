a12050db2908e5a463310e9ef17e3dad3474ffd5
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-29 13:13:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,20 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 
-var tokens = ["NjU5NDA1MzY2NjI2NjE1MzE3.Xghyjg.OqoHx2R76g7zre5M3yS3mAvF9aE", "NjU5NDI1ODk2MDQ1NzQwMDYy.Xghyow.BSZEG3wVAV-KKXU5PjrYu9wrdXI"]
-
-tokens.forEach(t => {
-let client = new Discord.Client();
-client.login(t);
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-  client.on("ready", () => {
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ client.on("ready", () => {
   const channel = client.channels.get("627884388452925453");
   if (!channel) return console.error("Can't find the channel.");
   channel.join().then(connection => {
     console.log("Connected.[Done]");
   }).catch(error_ => {
     console.error(error_);
- }
+  });
 });
-})
+client.login('NjU5NDA1MzY2NjI2NjE1MzE3.XgiYJQ.sCcWuI6gvvAx2Eq0xUaj0_CHp40')
+client2.login('NjU5NDI1ODk2MDQ1NzQwMDYy.XgiYQw.LpI435Fxl5Pe9IgRp3uOUGIOBGw')
