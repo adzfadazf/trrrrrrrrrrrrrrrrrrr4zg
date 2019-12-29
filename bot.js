cdd5d35d6ecaab935f049e4cb1e6f28a657331ec
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-29 08:06:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const Discord = require('discord.js');
-var tokens = ["NjYwNTMzNDg2MjU1MzQxNTc5.Xge7Kg.qMfbF23BmcEQvqm02x10U3uTVMA", "NTg0ODExNTgwMzExNjAxMTYy.Xge7-Q.uejm9DFzPcp1BpoxTXMWeKGUKEE", "token3"]
+const client = new Discord.Client();
 
-tokens.forEach(t => {
-let client = new Discord.Client();
-client.login(t);
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -19,5 +16,5 @@ client.on('ready', () => {
   });
 });
 
-
+client.login(process.env.BOT_TOKEN);
 
