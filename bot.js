117fcf9ba76586d9b37db4e1bb8d6d63fe48d9fb
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-29 18:08:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  const channel = client.channels.get("627884388452925453");
+  const channel = client.channels.get("529746544975544342");
   if (!channel) return console.error("Can't find the channel.");
   channel.join().then(connection => {
     console.log("Connected.[Done]");
