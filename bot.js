65888de6a54ec78827a24ab94539f72738d6eb83
adzfadazf <58477448+adzfadazf@users.noreply.github.com>
2019-12-29 13:16:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
- client.on("ready", () => {
+var tokens = ["NjU5NDI1ODk2MDQ1NzQwMDYy.XgiYQw.LpI435Fxl5Pe9IgRp3uOUGIOBGw", "NjU5NDA1MzY2NjI2NjE1MzE3.XgiZCA.UnPywdtsnG-jEsNN3LM4XI-e8vY"]
+
+tokens.forEach(t => {
+let client = new Discord.Client();
+client.login(t);
+
+client.on("ready", () => {
   const channel = client.channels.get("627884388452925453");
   if (!channel) return console.error("Can't find the channel.");
   channel.join().then(connection => {
@@ -16,5 +15,3 @@ client.on('ready', () => {
     console.error(error_);
   });
 });
-client.login('NjU5NDA1MzY2NjI2NjE1MzE3.XgiYJQ.sCcWuI6gvvAx2Eq0xUaj0_CHp40')
-client2.login('NjU5NDI1ODk2MDQ1NzQwMDYy.XgiYQw.LpI435Fxl5Pe9IgRp3uOUGIOBGw')
